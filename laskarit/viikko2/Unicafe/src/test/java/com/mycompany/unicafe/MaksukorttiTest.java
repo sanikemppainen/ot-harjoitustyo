9db2668bc47fd2ddfe0b5ef09bd5b89bc45f5e0d
sanikemppainen <sani.kemppainen@helsinki.fi>
2021-03-25 17:01:09 +0200

    maksukorttitest lisätään

--- laskarit/viikko2/Unicafe/src/test/java/com/mycompany/unicafe/MaksukorttiTest.java
+++ laskarit/viikko2/Unicafe/src/test/java/com/mycompany/unicafe/MaksukorttiTest.java
@@ -7,6 +7,7 @@ import org.junit.Test;
 public class MaksukorttiTest {
 
     Maksukortti kortti;
+    
 
     @Before
     public void setUp() {
@@ -16,5 +17,38 @@ public class MaksukorttiTest {
     @Test
     public void luotuKorttiOlemassa() {
         assertTrue(kortti!=null);      
+    }
+    @Test
+    public void alkusaldoOnOikein(){
+        assertEquals("saldo: 0.10", kortti.toString());
+    }
+    @Test
+    public void latausKasvattaaSaldoa(){
+        kortti.lataaRahaa(10);
+        assertEquals("saldo: 0.20", kortti.toString());
+    }
+    @Test
+    public void saldoVaheneeJosTarpeeksiRahaa(){
+        kortti.otaRahaa(2);
+        assertEquals("saldo: 0.8", kortti.toString());
+    }
+    @Test
+    public void saldoEiMuutuJosEiTarpeeksiRahaa(){
+        kortti.otaRahaa(200);
+        assertEquals("saldo: 0.10", kortti.toString());
+    }
+    @Test
+    public void onkoTarpeeksiRahaaPalauttaakoTrue(){
+        assertEquals(kortti.otaRahaa(3),true);
+    }
+    @Test
+    public void onkoTarpeeksiRahaaPalauttaakoFalse(){
+        assertEquals(kortti.otaRahaa(300),false);
+    }
+    @Test
+    public void kutsutaanSaldoa(){
+        kortti.lataaRahaa(3);
+        kortti.saldo();
+        assertEquals("saldo: 0.13", kortti.toString());
     }
 }
